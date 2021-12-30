bf94bfce44c5a490b351a67bc7b85a68b6fce7c1
Geemac2021 <83748336+Geemac2021@users.noreply.github.com>
2021-12-30 02:35:25 -0500

    Create Daily_Ass1.py

---
+++ Daily_Ass1.py
@@ -0,0 +1,20 @@
+#What is the correct syntax to print "Good morning folks" in python
+print("Good morning folks")
+
+#How do you insert comments in python code
+    #comment can be used either by using number sign.
+    ""or double quote""
+    """triple quote can also be used"""
+#How do you create a variable with the numeric value  5
+number = 5
+
+#What's the correct extension for python files
+""<file name>.py""
+#eg: 
+sample.py
+
+#How do you create a variable with the floating variable 8.6cd 
+
+x = 8.6
+print(x)
+
