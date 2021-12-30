c91ac5a508591f3a196d260dea8b5c1ea012476a
Geemac2021 <83748336+Geemac2021@users.noreply.github.com>
2021-12-30 03:59:34 -0500

    Create daily_Ass2.py

---
+++ daily_Ass2.py
@@ -0,0 +1,48 @@
+#MY DAILIES 2 - DEC 28, 2021
+#Question of the day: Tuesday December 28th
+
+#1. What is the result of print(type([]) is list)
+list = []
+print(type([]) is list)
+False
+
+
+#2. What is the output of the following variable assignment?
+x = 75
+def myfunc():
+    x = x + 1
+        print(x)
+        myfunc()
+        print(x)
+
+x = 75
+
+def myfunc(x):
+        x = x + 1
+        print(x)
+    myfunc(x)
+        print(x)
+                                                  
+76                                             
+
+#3. what is the output of the following code? print(bool(0), bool(3.14159), bool(-3), bool(1.0+1j))
+print(bool(0))
+False
+
+print(bool(3.14159))
+True
+
+print(bool(-3))
+True
+
+print(bool(1.0+1j))
+True
+
+
+#4. In Python 3, what is the output of type(range(5)). (What data type it will return).
+type(range(5))
+     range
+
+#5. What is the data type of print(type(10))
+print(type(10))
+<class 'int'>
