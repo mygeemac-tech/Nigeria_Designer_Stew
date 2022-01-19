2b13c112fb2091d204ec597ba2d5ea347efb2f60
geemac2021 <geeuwaifo@gmail.com>
2022-01-18 22:21:39 -0500

    new ass

---
+++ Daily_Ass6.py
@@ -0,0 +1,25 @@
+1. Which of the following is an operator and which is a value
+*
+'Hello'
+-88.88
+-
+/
++
+Operators: +, -, *, /
+Values: 'hello', -88.5, 5
+5
+2. Which of these is a variable and which is a string
+spam
+'spam'
+Variable: spam
+String: 'spam'
+
+Strings always start and end with quotes
+3. Name three data types
+Integers, floating-point numbers, and strings
+4. what is an expression made of. What do all expressions do?
+An expression is a combination of values and operators. All expressions evaluate (that is, reduce) to a single value.
+5. What does the variable bacon contain after running the the code below
+bacon = 20
+bacon + 1
+The bacon variable is set to 20. The bacon + 1 expression does not reassign the value in bacon (that would need an assignment statement: bacon = bacon + 1)
